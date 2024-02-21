e879651f6f6719d50af06e7c980cce9ea5492576
Jose <jafmgdam@gmail.com>
2024-02-21 15:11:09 +0100

    v2.9

--- app/src/main/java/com/example/hanzi/ui/theme/Theme.kt
+++ app/src/main/java/com/example/hanzi/ui/theme/Theme.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
+
 private val LightColors = lightColorScheme(
     primary = md_theme_light_primary,
     onPrimary = md_theme_light_onPrimary,
@@ -108,6 +109,7 @@ fun HanziTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
--- app/src/main/java/com/example/hanzi/ui/theme/Type.kt
+++ app/src/main/java/com/example/hanzi/ui/theme/Type.kt
@@ -2,19 +2,37 @@ package com.example.hanzi.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.hanzi.R
 
 // Set of Material typography styles to start with
+
+val Barlow = FontFamily(Font(R.font.barlow_regular))
+val Oswald = FontFamily(Font(R.font.oswald_regular))
+
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
+        fontSize = 30.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = Barlow,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Oswald,
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp
     )
+
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
